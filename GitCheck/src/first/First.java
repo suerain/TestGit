0bec781481f06ecdb0e03e3ff5451ca5a93eb419
suerain <surendralamichhane07@gmail.com>
2017-01-12 13:58:39 -0500

    Added exclaimation and displayed person

--- GitCheck/src/first/First.java
+++ GitCheck/src/first/First.java
@@ -3,9 +3,12 @@ package first;
 public class First {
 
 	public static void main(String[] args) {
-		System.out.println("First Class");
-		System.out.println("Changed");
-		System.out.println("Changed again");
+		System.out.println("First Class!");
+		System.out.println("Changed!");
+		System.out.println("Changed again!");
+		System.out.println("---------------");
+		Person p = new Person();
+		p.display();
 	}
 
 }
