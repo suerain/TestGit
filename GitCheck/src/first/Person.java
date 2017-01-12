70595ed2668a76c00f56359bec7268dfdbbacf48
suerain <surendralamichhane07@gmail.com>
2017-01-12 15:36:37 -0500

    Changed Defaults

--- GitCheck/src/first/Person.java
+++ GitCheck/src/first/Person.java
@@ -1,12 +1,13 @@
 package first;
 
 public class Person {
-	int[] data = { 0, 4, 23 };
+	int[] data = { -1, 0, 4, 23, 84 };
 
 	public void display() {
+		
 		System.out.println("Data: ");
-		for (int i = 0; i < data.length; i++) {
-			System.out.println("Next value: " + data[i]);
+		for (int i : data) {
+			System.out.println("Next value: " + i);
 		}
 	}
 
