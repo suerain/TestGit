aa7a994812cd44ad5500334fff0445384e819163
suerain <surendralamichhane07@gmail.com>
2017-01-12 15:41:26 -0500

    Displayed sum

--- GitCheck/src/first/Person.java
+++ GitCheck/src/first/Person.java
@@ -1,13 +1,18 @@
 package first;
-
+/*
+ * Person Class
+*/
 public class Person {
 	int[] data = { 0, 4, 23 };
+	int sum = 0;
 
 	public void display() {
 		System.out.println("Data: ");
 		for (int i = 0; i < data.length; i++) {
 			System.out.println("Next value: " + data[i]);
+			sum += data[i];
 		}
+		System.out.println("Sum is: " + sum);
 	}
 
 }
